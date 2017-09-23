7577244ea92767c5c6a7a5a0268da648d1c6001b
明-实验室PC <342875289@qq.com>
2017-09-23 12:45:08 +0800

    update

--- BIT/宣讲管理/excel.py
+++ BIT/宣讲管理/excel.py
@@ -19,7 +19,7 @@ class easyExcel:
             self.xlBook = self.xlApp.Workbooks.Add()    
             self.filename = ''  
         self.xlApp.Visible = False#操作过程不可见
-        #self.xlApp.DisplayAlerts = False#关闭错误提示
+        self.xlApp.DisplayAlerts = False#关闭错误提示
     def save(self, newfilename=None):  #保存文件  
         if newfilename:    
             self.filename = newfilename    
@@ -59,35 +59,24 @@ class easyExcel:
     def cpSheet(self, before):  #复制工作表  
         "copy sheet"     
         self.shts(1).Copy(None,shts(1))    
-#下面是一些测试代码。    
-if __name__ == "__main__":    
-    #PNFILE = r'c:/screenshot.bmp'  
+
+def readTemplate(filename_open,row_ignore=2,columns_ignore=3):
     try:
-        filename_open = 'template.xlsx'
-        filename_save = 'template2.xlsx'
-        row_ignore = 2
-        columns_ignore = 3
         xls = easyExcel(os.getcwd()+'\\'+filename_open)
         xls.setSheet('宣讲会教室借用')
-        
         schedule = {}
         class_list = []
         date_num  = xls.rows_conut/2 - 1
         class_num = xls.columns_conut - 3
-        
-        
-        print('读取到:%d行,%d列'% (xls.rows_conut,xls.columns_conut))
-        print('折合:%d天,%d个教室'% (date_num,class_num))
+        #print('读取到:%d行,%d列'% (xls.rows_conut,xls.columns_conut))
+        #print('折合:%d天,%d个教室'% (date_num,class_num))
         for class_info in range(1+columns_ignore,xls.columns_conut+1):
             class_list.append(xls.getCell(row_ignore,class_info))
-        print('教室列表:')
-        print(class_list)
-        
-
+        #print('教室列表:')
+        #print(class_list)
         for date_info in range(1+row_ignore,xls.rows_conut,2):
-        #for date_info in range(1+row_ignore,4,2):
             datetime_str = xls.getCell(date_info,'A').strftime('%Y-%m-%d')
-            print(xls.getCell(date_info,'A').strftime('%Y-%m-%d'))
+            #print(xls.getCell(date_info,'A').strftime('%Y-%m-%d'))
             schedule[datetime_str]={}
             schedule[datetime_str]['Morning']={}
             schedule[datetime_str]['Afternoon']={}
@@ -103,27 +92,43 @@ if __name__ == "__main__":
                     schedule[datetime_str]['Afternoon'][class_name]['isavailable'] = 0
                 else:
                     schedule[datetime_str]['Afternoon'][class_name]['isavailable'] = 1
-        
-        print(schedule)
-        
+        '''
         #将dict类型转化为str
         schedule_str = json.dumps(schedule)
-        
         #将str类型转化为dict
         schedule_dict = json.loads(schedule_str)
-
-        print(schedule_dict)
+        #print(schedule_dict)
         '''
-        print(xls.getCell(3,'C'))
-        print(xls.getCell(3,'D'))
-        print(xls.sht.Cells(3, 'D').Interior.ColorIndex)
-        print(xls.sht.Cells(3, 'L').Interior.ColorIndex)
-        xls.sht.Range('F3','F4').Merge()
-        xls.sht.Cells(3, 'F').Interior.ColorIndex=1
-        print(xls.sht.Range('C3','C4').MergeCells == True)
-        print(xls.sht.Range('F3','F4').MergeCells == True)
-        '''
-        #xls.save(os.getcwd()+'\\'+filename_save)    
-        print("finish")
     finally:
         xls.close() 
+    return [date_num,class_num,class_list,schedule]
+def cleanTemplate(filename_open,filename_save,row_ignore=2,columns_ignore=3):
+    try:
+        xls = easyExcel(os.getcwd()+'\\'+filename_open)
+        xls.setSheet('宣讲会教室借用')
+        for date_info in range(1+row_ignore,xls.rows_conut+1):
+            for class_info in range(1+columns_ignore,xls.columns_conut+1):
+                if xls.sht.Cells(date_info,class_info).Interior.ColorIndex != 1:
+                    xls.setCell(date_info, class_info, '')
+                    xls.sht.Cells(date_info,class_info).Interior.ColorIndex = 2
+        xls.save(os.getcwd()+'\\'+filename_save)
+    finally:
+       xls.close()
+       
+#下面是一些测试代码。    
+if __name__ == "__main__":    
+    #PNFILE = r'c:/screenshot.bmp'  
+    filename_open = 'template.xlsx'
+    filename_save = 'template2.xlsx'
+    row_ignore = 2
+    columns_ignore = 3
+    '''
+    [date_num,class_num,class_list,schedule] = readTemplate(filename_open,row_ignore,columns_ignore)
+    print('读取到:%d天,%d个教室'% (date_num,class_num))
+    print(class_list)
+    #print(schedule)
+    '''
+    cleanTemplate(filename_open,filename_save)
+    print("finish")
+        
+    #xls.close()
