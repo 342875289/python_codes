612db1dba4239cce4fdd2108bd12a1dbe53314fb
明-实验室PC <342875289@qq.com>
2017-09-25 21:14:00 +0800

    update

--- BIT/宣讲管理/test.py
+++ BIT/宣讲管理/test.py
@@ -51,6 +51,62 @@ print('有'+str(len(enterprise_list_confirmed))+'项申请已处理:')
 [date_num,class_num,class_list,schedule] = excel.reloadTemplate()
 #print('读取到:%d天,%d个教室'% (date_num,class_num))
 print(class_list)
+
+
+def query_schedule(enterprise,schedule):
+    #宣讲
+    if enterprise['date_display']:
+        try:
+            time = p_split_time.search(enterprise['time_display'])
+            if int(time.group(1)) <= 17:
+                time_part = 'Afternoon'
+            else:
+                time_part = 'Night'
+            for place in enterprise['place_display']:
+                #print(enterprise_new['date_display'])
+                #print(place_display)
+                if place[0] == 'F':
+                    time_part = 'Allday'
+                schedule_one = schedule[str(enterprise['date_display'])][time_part][place]
+                if schedule_one['isavailable'] :
+                    new_start = int(time.group(1))*100 + int(time.group(2))
+                    new_end = int(time.group(3))*100 + int(time.group(4))
+                    conflict = 0
+                    for enterprise_exist in schedule_one['enterprise_list']:
+                        exist_time = p_split_time.search(enterprise_exist['time'])
+                        exist_start = int(exist_time.group(1))*100 + int(exist_time.group(2))
+                        exist_end = int(exist_time.group(3))*100 + int(exist_time.group(4))
+                        if not (((new_start <= exist_start) and (new_start <= exist_end)) or ((new_end >= exist_start) and (new_end >= exist_end))):
+                            conflict = 1
+                    if conflict:
+                        print(enterprise)
+                        print('时间段冲突')
+                    else:
+                        one_case={}
+                        one_case['id'] = enterprise['id']
+                        one_case['name'] = enterprise['name']
+                        one_case['time'] = enterprise['time_display']
+                        one_case['type'] = 'display'
+                        schedule_one['enterprise_list'].append(one_case)
+                        #print("加入成功")
+                else:
+                    print('d所选时间段教师不可用:')
+                    print('name:%s,date:%s,class:%s,time:%s'% (enterprise['name'],enterprise['date_display'],enterprise['place_display'],enterprise['time_display'] ) )
+                    continue
+        except  KeyError as msg:#KeyError
+            print('d已分配的教室不在可用教室表中:'+enterprise['name']+'使用了'+str(msg))
+        except  AttributeError as msg:#KeyError
+            print('d详细时间未填写')
+        except  BaseException as msg:#KeyError
+            print('d错误信息:')
+            print(enterprise)
+            traceback.print_exc()
+            continue
+                    
+
+
+
+
 p_split_time = re.compile(r'(\d+):(\d+)-(\d+):(\d+)')
 for enterprise_new in enterprise_list_confirmed:
     #print(enterprise_new['name'])
@@ -73,7 +129,7 @@ for enterprise_new in enterprise_list_confirmed:
                     new_end = int(time.group(3))*100 + int(time.group(4))
                     conflict = 0
                     for enterprise_exist in schedule_one['enterprise_list']:
-                        exist_time = p_split_time.search(enterprise_exist['time_display'])
+                        exist_time = p_split_time.search(enterprise_exist['time'])
                         exist_start = int(exist_time.group(1))*100 + int(exist_time.group(2))
                         exist_end = int(exist_time.group(3))*100 + int(exist_time.group(4))
                         if not (((new_start <= exist_start) and (new_start <= exist_end)) or ((new_end >= exist_start) and (new_end >= exist_end))):
@@ -82,15 +138,21 @@ for enterprise_new in enterprise_list_confirmed:
                         print(enterprise_new)
                         print('时间段冲突')
                     else:
-                        schedule_one['enterprise_list'].append([enterprise_new['name'],enterprise_new['date_display'],enterprise_new['time_display'],enterprise_new['place_display']])
+                        one_case={}
+                        one_case['id'] = enterprise_new['id']
+                        one_case['name'] = enterprise_new['name']
+                        one_case['time'] = enterprise_new['time_display']
+                        one_case['type'] = 'display'
+                        schedule_one['enterprise_list'].append(one_case)
                         #print("加入成功")
                 else:
-                    print(enterprise_new)
-                    print('时间段错误')
+                    print('d所选时间段教师不可用:')
+                    print('name:%s,date:%s,class:%s,time:%s'% (enterprise_new['name'],enterprise_new['date_display'],enterprise_new['place_display'],enterprise_new['time_display'] ) )
                     continue
         except  KeyError as msg:#KeyError
-            print('d错误信息:'+repr(msg))
-            print(enterprise_new)
+            print('d已分配的教室不在可用教室表中:'+enterprise_new['name']+'使用了'+str(msg))
+        except  AttributeError as msg:#KeyError
+            print('d详细时间未填写')
         except  BaseException as msg:#KeyError
             print('d错误信息:')
             print(enterprise_new)
@@ -116,8 +178,8 @@ for enterprise_new in enterprise_list_confirmed:
                     new_end = int(time.group(3))*100 + int(time.group(4))
                     conflict = 0
                     for enterprise_exist in schedule_one['enterprise_list']:
-                        print(enterprise_exist)
-                        exist_time = p_split_time.search(enterprise_exist['time_writtenTest'])
+                        #print(enterprise_exist)
+                        exist_time = p_split_time.search(enterprise_exist['time'])
                         exist_start = int(exist_time.group(1))*100 + int(exist_time.group(2))
                         exist_end = int(exist_time.group(3))*100 + int(exist_time.group(4))
                         if not (((new_start <= exist_start) and (new_start <= exist_end)) or ((new_end >= exist_start) and (new_end >= exist_end))):
@@ -126,15 +188,21 @@ for enterprise_new in enterprise_list_confirmed:
                         print(enterprise_new)
                         print('时间段冲突')
                     else:
-                        schedule_one['enterprise_list'].append([enterprise_new['name'],enterprise_new['date_writtenTest'],enterprise_new['time_writtenTest'],enterprise_new['place_writtenTest']])
+                        one_case={}
+                        one_case['id'] = enterprise_new['id']
+                        one_case['name'] = enterprise_new['name']
+                        one_case['time'] = enterprise_new['time_writtenTest']
+                        one_case['type'] = 'writtenTest'
+                        schedule_one['enterprise_list'].append(one_case)
                         #print("加入成功")
                 else:
-                    print(enterprise_new)
-                    print('时间段错误')
+                    print('b所选时间段教师不可用:')
+                    print('name:%s,date:%s,class:%s,time:%s'% (enterprise_new['name'],enterprise_new['date_writtenTest'],enterprise_new['place_writtenTest'],enterprise_new['time_writtenTest'] ) )
                     continue
         except  KeyError as msg:#KeyErro
-            print('b错误信息:'+repr(msg))
-            print(enterprise_new)
+            print('b已分配的教室不在可用教室表中:'+enterprise_new['name']+'使用了'+str(msg))
+        except  AttributeError as msg:#KeyError
+            print('b详细时间未填写')
         except  BaseException as enterprise_exist:#KeyError
             print('b错误信息:')
             print(enterprise_new)
@@ -160,7 +228,7 @@ for enterprise_new in enterprise_list_confirmed:
                     new_end = int(time.group(3))*100 + int(time.group(4))
                     conflict = 0
                     for enterprise_exist in schedule_one['enterprise_list']:
-                        exist_time = p_split_time.search(enterprise_exist['time_faceTest'])
+                        exist_time = p_split_time.search(enterprise_exist['time'])
                         exist_start = int(exist_time.group(1))*100 + int(exist_time.group(2))
                         exist_end = int(exist_time.group(3))*100 + int(exist_time.group(4))
                         if not (((new_start <= exist_start) and (new_start <= exist_end)) or ((new_end >= exist_start) and (new_end >= exist_end))):
@@ -169,15 +237,22 @@ for enterprise_new in enterprise_list_confirmed:
                         print(enterprise_new)
                         print('时间段冲突')
                     else:
-                        schedule_one['enterprise_list'].append([enterprise_new['name'],enterprise_new['date'],enterprise_new['time_writtenTest'],enterprise_new['place_writtenTest']])
+                        one_case={}
+                        one_case['id'] = enterprise_new['id']
+                        one_case['name'] = enterprise_new['name']
+                        one_case['time'] = enterprise_new['time_display']
+                        one_case['type'] = 'faceTest'
+                        schedule_one['enterprise_list'].append(one_case)
                         #print("加入成功")
                 else:
-                    print(enterprise_new)
-                    print('时间段错误')
+                    print('f所选时间段教师不可用:')
+                    print('name:%s,date:%s,class:%s,time:%s'% (enterprise_new['name'],enterprise_new['date_faceTest'],enterprise_new['place_faceTest'],enterprise_new['time_faceTest'] ) )
+                    print('--------------')
                     continue
         except  KeyError as msg:#KeyError
-            print('f错误信息:'+repr(msg))
-            print(enterprise_new)
+            print('f已分配的教室不在可用教室表中:'+enterprise_new['name']+'使用了'+str(msg))
+        except  AttributeError as msg:#KeyError
+            print('f详细时间未填写')
         except  BaseException as msg:#KeyError
             print('f错误信息:')
             print(enterprise_new)
--- BIT/宣讲管理/test2.py
+++ BIT/宣讲管理/test2.py
@@ -1,8 +1,10 @@
-import re
-time = '17:00-17:30'
-p_time = re.compile(r'(\d+):(\d+)-(\d+):(\d+)')
-t = p_time.search(time)
-print(t.group(1))
-print(t.group(2))
-print(t.group(3))
-print(t.group(4))
+a=[]
+
+def t(a):
+    a.append('1')
+    print(a)
+    
+
+t(a)
+t(a)
+print(a)
