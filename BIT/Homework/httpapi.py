286acadfcd9acb8c190418e3152684f8cf6dc4d6
明-实验室PC <342875289@qq.com>
2017-10-17 17:20:31 +0800

    update

--- BIT/Homework/httpapi.py
+++ BIT/Homework/httpapi.py
@@ -1,6 +1,7 @@
 import urllib.request
 import http.cookiejar
 import json
+import ssl
 
 ip1  = '123.207.255.131'
 ip2  = '127.0.0.1'
@@ -10,12 +11,12 @@ cookieJarInMemory = http.cookiejar.CookieJar();
 opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookieJarInMemory));
 urllib.request.install_opener(opener);
 
-
+ssl._create_default_https_context = ssl._create_unverified_context
 param ={'username':'test',
         'password':'test',
         }
 post_data_code= urllib.parse.urlencode(param).encode(encoding='UTF8')
-url='http://'+ip+':8000/login'
+url='https://'+ip+'/login'
 request = urllib.request.Request(url)
 request.add_header('Accept', '*/*')
 request.add_header('Content-Type',' application/x-www-form-urlencoded')
@@ -23,8 +24,8 @@ response = urllib.request.urlopen(request,data=post_data_code)
 context = response.read().decode('utf-8')
 print(context)
 
-'''
-url='http://'+ip+':8000/book_list'
+
+url='https://'+ip+'/book_list'
 request = urllib.request.Request(url)
 request.add_header('Accept', '*/*')
 request.add_header('Content-Type',' application/x-www-form-urlencoded')
@@ -32,20 +33,20 @@ response = urllib.request.urlopen(request)
 context = response.read().decode('utf-8')
 print(context)
 
-
+'''
 param ={'book_id':4,
         'times':1,
         }
 post_data_code= urllib.parse.urlencode(param).encode(encoding='UTF8')
-url='http://'+ip+':8000/purchase'
+url='https://'+ip+':7000/purchase'
 request = urllib.request.Request(url)
 request.add_header('Accept', '*/*')
 request.add_header('Content-Type',' application/x-www-form-urlencoded')
 response = urllib.request.urlopen(request,data=post_data_code)
 context = response.read().decode('utf-8')
 print(context)
-
 '''
+
 '''
 param ={'book_id':4,
         }
@@ -59,6 +60,7 @@ context = response.read().decode('utf-8')
 print(context)
 
 '''
+'''
 param ={'book_id':4,
         'mac':'91-90-96-D9-9B-07'
         }
@@ -70,6 +72,6 @@ request.add_header('Content-Type',' application/x-www-form-urlencoded')
 response = urllib.request.urlopen(request,data=post_data_code)
 context = response.read().decode('utf-8')
 print(context)
-
+'''
 
     
